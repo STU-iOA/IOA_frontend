9e3a52564361e6545d6a2a86e8195b5d5e369e6c
Demon789 <3199885084@qq.com>
2021-12-06 19:58:31 +0800

    加入缓存

--- pages/Me/axios/Me/Me.js
+++ pages/Me/axios/Me/Me.js
@@ -1,25 +1,5 @@
 import api from "../interfaceAPIs/MeAPI/api.js"
 import axios from "../axios.interceptors.js";
-
-// try{
-//   await new Promise((resolve,reject)=>{
-//    uni.getStorage({
-//     key:'token',
-//     success: function(res) {
-//      this.token =res.data;
-//      resolve(1);
-//     },
-//     fail() {
-//      console.log("您还没有登陆");
-//      reject(1);
-//     }
-//    })
-//    })
-//  }catch(e){
-//   console.log("您还没有登陆");
-//   //TODO handle the exception
-//  }
-
 //获取用户校园网账号
 export function getUserAccount(data) {
 	return axios.post(api.post.getUserAccount,data)
